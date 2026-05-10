1f9b54cd5507c2aedebc5519968190c46c9862e7
eostendarp <eostendarp@gmail.com>
2026-05-10 05:14:03 +0000

    enable docker desktop

--- config.nix
+++ config.nix
@@ -1,6 +1,7 @@
 { pkgs, inputs, ... }: {
   wsl.enable = true;
   wsl.defaultUser = "eostendarp";
+  wsl.docker-desktop.enable = true;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nix.settings.auto-optimise-store = true;
